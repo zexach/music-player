dd5b212c205f282bee2c44d69b123e75a9b6fcc9
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2023-06-30 18:16:25 +0200

    Added Spotify OAuth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,41 @@ import axios from "axios";
 const Home = () => {
 
     const clientID = import.meta.env.VITE_CLIENT_ID;
-    const secretID = import.meta.env.VITE_SECRET_ID;
     const redirectURI = import.meta.env.VITE_REDIRECT_URI;
     const responseType = import.meta.env.VITE_RESPONSE_TYPE;
 
-    const URL = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&responseType=${responseType}`;
+    const [token, setToken] = useState({});
 
-    const handleClick = async() => {
-        window.open(URL, 'popup', 'popup=true');
+    const URL = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`;
+
+
+    const getDataFromURL = () => {
+
+        const data = window.location.hash.substring(1).split('&').reduce((initial, item) => {
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            
+            return initial;
+        }, {})
+
+
+        return data;      
     }
+
+    useEffect(() => {
+
+        console.log(getDataFromURL());
+
+        window.localStorage.setItem('token', getDataFromURL().access_token);
+        window.localStorage.setItem('type', getDataFromURL().token_type);
+        window.localStorage.setItem('expiresIn', getDataFromURL().expires_in);
+
+
+    }, [])
 
     return(
         <div className="home">
-            <button onClick={handleClick} className="btn">Fetch</button>
+            <a href={URL}>Login to spotify</a>
         </div>
     )
 }
