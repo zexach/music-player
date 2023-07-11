b4e73c289ed6707f3aa6e65af3bc37648e41931d
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2023-07-11 23:49:18 +0200

    Added style for SingleTrack

--- src/components/SingleTrack.jsx
+++ src/components/SingleTrack.jsx
@@ -5,7 +5,14 @@ const SingleTrack = (props) => {
 
     return(
         <>
-            <div>emir</div>
+            <div className="single-track">
+                <div className="single-track__details">
+                    <img src="https://i.scdn.co/image/ab67616d0000b273e14f11f796cef9f9a82691a7" className="single-track__details__img" alt="track" />
+                    <p className="single-track__details__name">Wake me up</p>
+                </div>
+                <p className="single-track__details__name">123.456.78</p>
+                <p className="single-track__details__name">4:56</p>
+            </div>
         </>
     );
 }
